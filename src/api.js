9ddf183fc418ef1eef5448d54c7e42d64f57c87a
Ulan <ahmatbekov95@gmail.com>
2022-11-24 17:30:54 +0600

    initial app

--- src/api.js
+++ src/api.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const serverless = require('serverless-http')
+const bodyParser = require('body-parser')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/index')
 
 const app = express()
-const router = express.Router()
 
-router.get('/', (req, res) => {
-	res.json({
-		hello: 'hi!',
+app.use(bodyParser.json())
+app.use(
+	'/',
+	graphqlHTTP({
+		schema: schema,
+		graphiql: true,
 	})
-})
-
-app.use(`/`, router)
+)
 
 module.exports = app
 module.exports.handler = serverless(app)
